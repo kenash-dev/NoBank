f746342898bcbf53507b498cf2f11e26df3483e6
kenash-dev <44662307+kenash-dev@users.noreply.github.com>
2021-11-20 23:19:03 +1100

    Add DbUp project.

---
+++ NoBank.Database/NoBank.Database/Program.cs
@@ -0,0 +1,42 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NoBank.Database
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+        args.FirstOrDefault()
+        ?? "Server=(local)\\SqlExpress; Database=NoBank; Trusted_connection=true";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ NoBank.Domain/NoBank.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NoBank.Domain
+{
+    public class Class1
+    {
+    }
+}
